4f909f94d801d228e496c80e4350d907bb4c8ea7
Jay Surface <jaypandya2k5+surface@gmail.com>
2020-01-18 14:24:41 -0500

    switched to eslint

---
+++ server/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:react/recommended", "standard"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-unused-vars": 0,
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "always-multiline"
+      }
+    ],
+    "arrow-parens": ["error", "always"],
+    "space-before-function-paren": ["error", "never"],
+    "@typescript-eslint/explicit-member-accessibility": 2
+  }
+};
