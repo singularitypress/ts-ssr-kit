910fc51b8cf999354443300e7aabfcf4c594f9f4
jaypandya2k5+laptop <jaypandya2k5+laptop@gmail.com>
2020-05-16 14:36:07 -0400

    not going to use foundation's JS

--- server/src/helpers/renderer.tsx
+++ server/src/helpers/renderer.tsx
@@ -22,7 +22,6 @@ export const renderer = (req: any, store: any) => {
       <body>
         <div id="root">${content}</div>
         <script defer src="bundle.js"></script>
-        <script defer src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js" integrity="sha256-pRF3zifJRA9jXGv++b06qwtSqX1byFQOLjqa2PTEb2o=" crossorigin="anonymous"></script>
       </body>
     </html>
   `;
