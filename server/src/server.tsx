1979b7f831c3e054f3f2c2909e9b36c3167f1a11
jaypandya2k5+laptop <jaypandya2k5+laptop@gmail.com>
2020-05-19 14:46:11 -0400

    add proxy params

--- server/src/server.tsx
+++ server/src/server.tsx
@@ -1,4 +1,5 @@
 import * as express from "express";
+import * as proxy from "express-http-proxy";
 import * as compression from "compression";
 import { matchRoutes } from "react-router-config";
 import { renderer, setStore } from "./helpers";
@@ -7,15 +8,22 @@ import Routes from "./Routes";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// {4}
 app.use(compression());
+// {5}
 app.use(express.static("public"));
+// {6}
+app.use("/api", proxy("http://react-ssr-api.herokuapp.com", {
+  proxyReqOptDecorator: (opts) => {
+    opts.headers["x-forwarded-host"] = "localhost:3000";
+    return opts;
+  },
+}));
 
 app.get("*", (req: any, res: any) => {
   const store = setStore();
 
-  /**
-   * {1}
-   */
+  // {1}
   const promises = matchRoutes(Routes, req.path).map((matchedRoute) => {
     const { route } = matchedRoute;
     return route.loadData ? route.loadData(store) : null;
