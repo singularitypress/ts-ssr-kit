fedb0f6ced3193e1b215377986775fb180054d85
singularitypress <jaypandya2k5@gmail.com>
2020-08-07 10:34:22 -0400

    remove unnecessary loaders

--- webpack.server.js
+++ webpack.server.js
@@ -4,14 +4,13 @@ const webpackNodeExternals = require("webpack-node-externals");
 
 const BUILD_DIR = path.resolve(__dirname, "build");
 const APP_DIR = path.resolve(__dirname, "src");
-const STYLE_DIR = path.resolve(__dirname, "style");
 const NODE_MODULES = path.resolve(__dirname, "node_modules");
 const NAME = "bundle";
 
 var config = () => {
 	return {
 		target: "node",
-		entry: [`${APP_DIR}/server.tsx`, `${STYLE_DIR}/index.scss`],
+		entry: [`${APP_DIR}/server.tsx`],
 		resolve: {
 			extensions: [".ts", ".js", ".json", ".tsx", ".jsx"],
 		},
@@ -32,39 +31,7 @@ var config = () => {
 					test: /\.js$/,
 					include: APP_DIR,
 					loader: "source-map-loader",
-				},
-				{
-					test: /\.scss$/,
-					use: [
-						{
-							loader: "file-loader",
-							options: {
-								name: `${NAME}.css`,
-								context: BUILD_DIR,
-								outputPath: "./",
-								publicPath: "../",
-							},
-						},
-						{
-							loader: "extract-loader",
-						},
-						{
-							loader: "css-loader",
-							options: {
-								sourceMap: false,
-							},
-						},
-						{
-							loader: "postcss-loader",
-						},
-						{
-							loader: "sass-loader",
-							options: {
-								sourceMap: true,
-							},
-						},
-					],
-				},
+				}
 			],
 		},
 		devServer: {
