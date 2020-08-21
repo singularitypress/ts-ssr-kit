02687c5f3c098dce482c710d4b15928c099a951d
singularitypress <jaypandya2k5@gmail.com>
2020-08-20 20:38:51 -0400

    setting up the hero

---
+++ src/client/components/Hero.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export const Hero = () => {
+  const gridClassName = "w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4";
+  return (
+    <div className="">
+
+    </div>
+  );
+};
