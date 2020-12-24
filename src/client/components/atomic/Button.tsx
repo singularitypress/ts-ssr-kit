5f551ddcc5bbaa338a13df327ae3018509c3da96
jaypandya2k5+laptop <jaypandya2k5+laptop@gmail.com>
2020-12-24 15:36:53 -0500

    reworked existing code to work with tailwind darkmode

--- src/client/components/atomic/Button.tsx
+++ src/client/components/atomic/Button.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 
 export const Button = (props: { children: React.ReactNode }) => {
   const shape = "pt-3 pb-3 pr-6 pl-6 rounded-full";
-  const colour = "bg-indigo-400 hover:bg-indigo-500";
+  const text = "text-white hover:text-black dark:text-black dark:hover:text-white font-semibold";
+  const border = "border-2 border-solid border-black dark:border-white";
+  const colour = "bg-black hover:bg-white dark:bg-white dark:hover:bg-black";
   const animate = "transition duration-300 ease-in-out";
   return (
-    <button className={`${animate} ${colour} ${shape}`}>{props.children}</button>
+    <button className={`${animate} ${text} ${border} ${colour} ${shape}`}>{props.children}</button>
   );
 };
