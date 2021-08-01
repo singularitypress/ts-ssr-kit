b0e6ff1ae8e28f33353485e0ccfe9aa0aa9fb422
Jay Pandya <jaypandya2k5@gmail.com>
2021-08-01 19:55:15 -0400

    add ws query to its own util, fix server watch, add tailwind forms

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -1,54 +1,43 @@
-import axios from "axios";
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import { createClient } from "graphql-ws";
 
 import { Hero } from "../components";
 import { Button } from "../components/atomic";
+import { wsQuery } from "../utils";
 import { IRes, ITransaction } from "../../types";
 
 const { useState, useEffect } = React;
 
+interface IQueryArgs {
+  startDate: string
+  endDate: string
+  include: string[]
+  exclude: string[]
+  account: string
+  institution: string
+  monthly: boolean
+}
+
 const _Home = () => {
   const [transactions, setTransactions] = useState([] as ITransaction[]);
   const [keyword, setKeyword] = useState("");
+  const [fragment, setFragment] = useState(["description", "amount", "date"]);
 
-  const query = `{\n  transactions(include: "${keyword}") {\n    ...transactionInfo\n  }\n}\n\nfragment transactionInfo on Transaction {\n  description, amount, date\n}`;
+  const query = `
+    {
+      transactions(include: "${keyword}") {
+        ...transactionInfo
+      }
+    }
+    fragment transactionInfo on Transaction {
+      ${fragment.join(",")}
+    }`;
   const next = (res: IRes) => { setTransactions(res.data.transactions); console.log(transactions); };
 
   useEffect(() => {
     console.log(transactions);
   });
-
-  const requestTransactions = () => {
-    axios({
-      method: "POST",
-      url: "http://localhost:4000/graphql",
-      data: { query, variables: null },
-    }).then((res) => {
-      setTransactions(res.data.data.transactions);
-    });
-  };
-
-  const client = createClient({
-    url: "ws://localhost:4001/graphql",
-  });
-
-  const wsQuery = async () => {
-    await new Promise((resolve, reject) => {
-      let result: any;
-      client.subscribe(
-        {
-          query,
-        },
-        {
-          next,
-          error: reject,
-          complete: () => resolve(result),
-        },
-      );
-    });
-  };
 
   return (
     <>
@@ -57,9 +46,12 @@ const _Home = () => {
       </Helmet>
       <Hero video={{ src: "https://i.imgur.com/FvaJnWc.mp4" }} height="100vh">
         <h1 className="prose prose-headline text-center">Welcome Home</h1>
-        <input style={{ padding: "1rem", borderRadius: "999px", color: "#333" }} type="text" onChange={(e) => setKeyword(e.target.value)} />
-        <br />
-        <Button onClick={wsQuery}>graphql check</Button>
+        <form onSubmit={(e) => { e.preventDefault(); }}>
+          <input style={{ padding: "1rem", borderRadius: "999px", color: "#333" }} type="text" onChange={(e) => setKeyword(e.target.value)} />
+
+          <br />
+          <Button onClick={() => wsQuery(query, next)}>graphql check</Button>
+        </form>
       </Hero>
     </>
   );
