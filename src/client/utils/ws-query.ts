26d61776203ffb4e1d609b56a23be1ef80c5e7f2
Jay Pandya <jaypandya2k5@gmail.com>
2021-09-06 13:12:12 -0400

    fix window condition for wsquery util

--- src/client/utils/ws-query.ts
+++ src/client/utils/ws-query.ts
@@ -1,7 +1,7 @@
 import { createClient } from "graphql-ws";
 
 export const wsQuery = async (query: string, next: any) => {
-  if (window !== "undefined") {
+  if (typeof window !== "undefined") {
     const client = createClient({
       url: "ws://localhost:4001/graphql",
     });
